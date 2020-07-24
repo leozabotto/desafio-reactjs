eb40b5e12caf1a9f10de2b061fc2213ae627cb9e
Leonardo Zabotto <leozabotto02@gmail.com>
2020-07-24 20:56:11 -0300

    Challenge Completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from 'services/api'
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      url: 'https://github.com/leozabotto/Aurora',
+      title: 'Aurora',
+      techs: [
+        'HTML',
+        'CSS',
+        'MySQL',
+        'PHP'
+      ]
+    })
+
+    setRepositories([...repositories, response.data])
+
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
+    
   }
+
+  useEffect(() => {
+    api.get('/repositories')
+    .then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repository => {
+          return (
+            <li key={repository.id}>  
+              {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+            </li>)
+        })}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
